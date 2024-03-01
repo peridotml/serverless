7336c71f34cb802c65c6f05a51dd5168048f1c43
Evan Sadler <easadler@gmail.com>
2024-03-01 12:18:02 -0500

    adds a gke cluster

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 terraform {
   backend "gcs" {
-    bucket      = "atlantis-demo"
-    prefix      = "terraform"
+    bucket = "atlantis-demo"
+    prefix = "terraform"
   }
 
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.51.0"
     }
   }
@@ -31,4 +31,47 @@ resource "google_project_service" "cloudapis" {
 resource "google_project_service" "containerregistry" {
   service            = "containerregistry.googleapis.com"
   disable_on_destroy = false
+}
+
+resource "google_project_service" "containerregistry" {
+  service            = "containerregistry.googleapis.com"
+  disable_on_destroy = false
+}
+
+
+resource "google_container_cluster" "knative_cluster" {
+  name     = "knative-cluster"
+  location = "us-east1-b"
+
+  initial_node_count = 1
+  min_master_version = "latest"
+
+  node_config {
+    machine_type = "e2-small" # Adjust based on your app's needs
+    spot         = true       # Use Spot VMs for cost efficiency
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 6 # Adjust based on expected load
+  }
+
+  network    = "default"
+  subnetwork = "default"
+
+  addons_config {
+    http_load_balancing {
+      disabled = false
+    }
+  }
+
+  # Assuming you're managing logging and monitoring elsewhere to control costs
+  logging_service    = "none"
+  monitoring_service = "none"
+
+  depends_on = [google_project_service.gke, google_project_service.cloudapis, google_project_service.containerregistry]
 }
