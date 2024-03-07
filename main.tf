72e860c3dff9e77b5d6f1779cc8541de87d49878
Evan Sadler <easadler@gmail.com>
2024-03-07 10:37:02 -0500

    oopsie

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "google_container_node_pool" "primary_nodes" {
   node_count = 1
 
   node_config {
-    machine_type = "e2-small"
+    machine_type = "e2-micro"
     spot         = true
 
     oauth_scopes = [
